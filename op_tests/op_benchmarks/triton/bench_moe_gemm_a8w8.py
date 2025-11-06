7dbe4735c5b64fff4b11f61ec376e64e546999cb
Nicholas Susanto <nicholas.susanto@amd.com>
2025-11-06 11:44:24 -0600

    Add MoE GEMM a8w8 + unit test and benchmark

---
+++ op_tests/op_benchmarks/triton/bench_moe_gemm_a8w8.py
@@ -0,0 +1,206 @@
+# adapted from triton_kernels package
+# original code https://github.com/triton-lang/triton/blob/main/python/triton_kernels/bench/bench_mlp.py
+
+from itertools import chain
+from pathlib import Path
+from copy import deepcopy
+import csv
+import triton.profiler as proton
+import torch
+import argparse
+from aiter.ops.triton.moe_routing.routing import routing
+from aiter.ops.triton.gemm_a16w16 import gemm_a16w16
+from aiter.ops.triton.moe_op_gemm_a8w8 import moe_gemm_a8w8, swizzle_scales, downcast_to_static_fp8
+from aiter.ops.triton.utils._triton.arch_info import get_arch
+import tempfile
+from triton_kernels.numerics_details.mxfp import downcast_to_mxfp
+import inspect
+
+
+def parse_profile(profile_path, useful_op_regex, reps):
+    """
+    construct a PerfRecord from a (proton) profile path and a regex for useful operations
+    """
+    from triton.profiler import viewer
+    gf, _, _, _ = viewer.read(profile_path)
+    # aggregate "useful" flops + bytes
+    useful = gf.filter(f"MATCH ('*', c) WHERE c.'name' =~ '{useful_op_regex}' AND c IS LEAF").dataframe
+    bytes = int(useful["bytes"].sum())
+    flops = int(sum(useful[[c for c in ["flops8", "flops16"] if c in useful.columns]].sum()))
+    # take all ops (incl. "not useful" ones) when computing total time
+    allops = gf.filter("MATCH ('*', c) WHERE c IS LEAF").dataframe
+    total_time_ns = allops["time (ns)"].sum()
+    kernel_time_ns = useful["time (ns)"].sum()
+    return {"total_time_ns": total_time_ns, "kernel_time_ns": kernel_time_ns, "flops": flops, "bytes": bytes, "reps": reps}
+
+
+def compute_roofline(*args, \
+                  bench_fn, intensity_proxy_name, intensity_proxy_values, out_path, \
+                  **kwargs):
+    # validate input args
+    if not isinstance(intensity_proxy_name, str):
+        raise TypeError("intensity_proxy must be a string naming a parameter in target_fn")
+    # determine position of intensity_proxy in target_fn signature
+    sig = inspect.signature(bench_fn)
+    params = list(sig.parameters.values())
+    if intensity_proxy_name not in sig.parameters:
+        raise ValueError(f"Parameter '{intensity_proxy_name}' not found in {bench_fn.__name__} signature")
+    pos_index = [p.name for p in params].index(intensity_proxy_name)
+
+    # wrapper to inject intensity proxy into target_fn and call it
+    def inject_proxy_and_call(val, args, kwargs):
+        args_list = list(args)
+        args_list.insert(pos_index, val)
+        return bench_fn(*args_list, **kwargs)
+
+    # collect performance data
+    perfs = []
+    print("=========================================")
+    print(f"{out_path   }...")
+    print("=========================================")
+    for val in intensity_proxy_values:
+        perf = inject_proxy_and_call(val, args, kwargs)
+        perfs.append(perf)
+        tflops = perfs[-1]["flops"] / perfs[-1]["kernel_time_ns"] * 1e-3
+        tbps = perfs[-1]["bytes"] / perfs[-1]["kernel_time_ns"] * 1e-3
+        total_latency = perfs[-1]["total_time_ns"] / 1e3 / perfs[-1]["reps"]
+        kernel_latency = perfs[-1]["kernel_time_ns"] / 1e3 / perfs[-1]["reps"]
+        print(f"{intensity_proxy_name}: {val:5d} | Total latency (us): {total_latency:.2f} | Kernel latency (us): {kernel_latency:.2f} | TFLOPS: {tflops:#.4g} | TBPS: {tbps:.2f}")
+
+
+def check_and_swizzle_scales(scale, N, K):
+    if N % 32 == 0 and K % (32 * 8) == 0:
+        scale = swizzle_scales(scale)
+        return scale, "CDNA4_SCALE"
+    else:
+        return scale, None
+
+
+def quantize(x, dtype):
+    if dtype == "bf16":
+        x = x.to(torch.bfloat16).transpose(-1, -2).contiguous().transpose(-1, -2)
+        return x, None
+    elif dtype == "fp8":
+        scale = x.abs().max().item() / 448.0
+        fp8e4_dtype = torch.float8_e4m3fn if get_arch() != "gfx942" else torch.float8_e4m3fnuz
+        x = x.to(fp8e4_dtype)
+        return x, scale
+    elif dtype == "mx8":
+        fp8e4_dtype = torch.float8_e4m3fn if get_arch() != "gfx942" else torch.float8_e4m3fnuz
+        x, scale = downcast_to_mxfp(x.to(torch.bfloat16), fp8e4_dtype, axis=1)
+        return x, scale
+    else:
+        assert dtype == "mx4", f"{dtype=}"
+        x, scale = downcast_to_mxfp(x.to(torch.bfloat16), torch.uint8, axis=1)
+        return x, scale
+    
+
+def bench_mlp(batch, dim1, dim2, n_expts_tot, n_expts_act, x_dtype, w_dtype, TP, op_regex):
+    rank = 0
+    dev = f"cuda:{rank}"
+
+    assert dim2 % TP == 0, f"{dim2=}, {TP=}, dim2 must be divisible by TP"
+
+    # -- init data --
+    # weights
+    wg = torch.randn((dim1, n_expts_tot), device=dev)
+    w1 = torch.randn((n_expts_tot, dim1, dim2 // TP), device=dev)
+    w2 = torch.randn((n_expts_tot, dim2 // TP // 2, dim1), device=dev)
+    # biases
+    bg = torch.randn((n_expts_tot, ), device=dev)
+    b1 = torch.randn((n_expts_tot, dim2 // TP), device=dev)
+    b2 = torch.randn((n_expts_tot, dim1), device=dev)
+
+    # -- numerics --
+    wg, _ = quantize(wg, "bf16")
+    w1, w1_scale = quantize(w1, w_dtype)
+    w2, w2_scale = quantize(w2, w_dtype)
+    w1_scale, swizzle_mx_scale1 = check_and_swizzle_scales(w1_scale, dim2 // TP, dim1)
+    w2_scale, swizzle_mx_scale2 = check_and_swizzle_scales(w2_scale, dim1, dim2 // TP // 2)
+
+    # -- benchmark --
+    x_dtype_str = x_dtype
+    x_dtype = torch.float8_e4m3fn
+    # special treatment of fp8_e4m3 on AMD CDNA3 because it uses fp8_e4m3fnuz
+    if x_dtype == torch.float8_e4m3fn and get_arch() == "gfx942":
+        x_dtype = torch.float8_e4m3fnuz
+
+    reps = 100
+    x = torch.randn((batch, dim1), dtype=torch.bfloat16, device=dev)
+    xg = x
+    if x_dtype_str == "fp8":
+        static_scale = torch.tensor(1e-4, device=dev)
+    # run layer
+    fpath = Path(tempfile.mktemp())
+    proton.start(str(fpath), hook="triton")
+    for i in range(reps):
+        logits = gemm_a16w16(xg, wg.T, bg)
+        rdata, gather_indx, scatter_indx = routing(logits, n_expts_act)
+        if x_dtype_str == "fp8":
+            x = downcast_to_static_fp8(x, static_scale)
+            x = moe_gemm_a8w8(x, w1, None, w1_scale, static_scale, static_scale, b1, rdata, gather_indx=gather_indx, swizzle_mx_scale=swizzle_mx_scale1, out_dtype=x_dtype, apply_swiglu=True)
+            x = moe_gemm_a8w8(x, w2, None, w2_scale, static_scale, None, b2, rdata, scatter_indx=scatter_indx, swizzle_mx_scale=swizzle_mx_scale2)
+        else:
+            assert x_dtype_str == "mx8"
+            x, _, x_scale = quantize(x, x_dtype_str)
+            x = moe_gemm_a8w8(x, w1, x_scale, w1_scale, None, None, b1, rdata, gather_indx=gather_indx, swizzle_mx_scale="CDNA4_SCALE", apply_swiglu=True)
+            x, _, x_scale = quantize(x, x_dtype_str)
+            x = moe_gemm_a8w8(x, w2, x_scale, w2_scale, None, None, b2, rdata, scatter_indx=scatter_indx, swizzle_mx_scale="CDNA4_SCALE")
+    proton.finalize()
+    return parse_profile(fpath.with_suffix(".hatchet"), useful_op_regex=op_regex, reps=reps)
+
+
+def roofline_mlp(batch_sizes, dim1, dim2, n_expts_tot, n_expts_act, x_dtype, w_dtype, TP, op_regex, name=""):
+    out_path = Path(f"logs/{name}/{x_dtype}x-{w_dtype}w-TP{TP}/")
+    out_path.mkdir(parents=True, exist_ok=True)
+    csv_path = compute_roofline(dim1, dim2, n_expts_tot, n_expts_act, x_dtype, w_dtype, TP, op_regex,  # fixed args
+                                         bench_fn=bench_mlp,  # function to benchmark
+                                         intensity_proxy_name="batch",  # intensity proxy name
+                                         intensity_proxy_values=batch_sizes,  # intensity proxy values to sweep
+                                         out_path=out_path.with_suffix(".csv"))  # output path
+                                
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(prog="Benchmark MoE")
+    parser.add_argument(
+        "--shape",
+        type=int,
+        nargs="+",
+        metavar=("DIM"),
+        help="Input feature dimensions of MoE layers. Must be two integers.",
+    )
+    parser.add_argument(
+        "--experts",
+        type=int,
+        nargs="+",
+        metavar=("DIM"),
+        help="Number of total and active experts in [total experts, active experts] order.",
+    )
+    parser.add_argument(
+        "--op-regex",
+        type=str,
+        default=".*moe_gemm.*",
+        help="Regex to find perf for specific operation by its kernel name.",
+    )
+    parser.add_argument(
+        "--act-dtype",
+        type=str,
+        default="fp8",
+        help="Activation dtype, fp8 or mx8.",
+    )
+    args = parser.parse_args()
+    return args
+
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    dim1, dim2 = args.shape
+    total_experts, active_experts = args.experts
+    batch_ranges_moe = [(1, 2, 1), (2, 5, 2), (8, 18, 8), (32, 65, 32), (128, 257, 128), (1024, 1200, 200), (4096, 8200, 4096)]
+    batch_sizes_moe = list(chain(*[range(*r) for r in batch_ranges_moe]))
+    quantized_dtypes = [args.act_dtype, "mx8"] 
+
+    roofline_mlp(batch_sizes_moe, dim1, dim2, total_experts, active_experts, quantized_dtypes[0], quantized_dtypes[1], TP=1, op_regex=args.op_regex, name="gpt-oss-x2")
---
+++ op_tests/triton_tests/test_moe_gemm_a8w8.py
@@ -0,0 +1,236 @@
+# adapted from triton_kernels package
+# original code https://github.com/triton-lang/triton/blob/main/python/triton_kernels/tests/test_matmul.py
+
+from dataclasses import dataclass, fields
+import itertools
+import pytest
+import torch
+from typing import Union
+import triton
+# routing utilities
+from aiter.ops.triton.moe_routing.routing import routing
+# matmul utilities
+from aiter.ops.triton.moe_op_gemm_a8w8 import moe_gemm_a8w8, moe_gemm_torch, swizzle_scales, downcast_to_static_fp8
+# numerics utilities
+from triton_kernels.numerics_details.mxfp import downcast_to_mxfp, upcast_from_mxfp
+# target-specific utilities
+from aiter.ops.triton.utils._triton.arch_info import get_arch
+
+# ---------------
+# initialize data
+# ---------------
+
+
+def alloc_rand(shape, device, dtype):
+    if dtype.itemsize == 1:
+        tmp = 2**-(torch.randint(4, 8, shape, device=device, dtype=torch.bfloat16))
+        return tmp
+    return torch.randn(shape, device=device, dtype=dtype)
+
+
+def alloc_rand_like(x):
+    return alloc_rand(x.shape, x.device, x.dtype)
+
+
+def init_routing_data(m, n_expts_tot, n_expts_act, do_gather, do_scatter, device="cuda"):
+    logits = torch.randn((m, n_expts_tot), dtype=torch.float16, device=device)
+    routing_data, gather_idx, scatter_idx = routing(logits, n_expts_act)
+    routing_data.gate_scal = None
+    gather_idx = gather_idx if do_gather else None
+    scatter_idx = scatter_idx if do_scatter else None
+    # TODO: re-enable
+    # if do_gather and do_scatter and n_expts_act == 1 and n_expt_shards == 1:
+    #     scatter_idx = mask_indx(scatter_idx, n_expts_act)
+    return m, routing_data, gather_idx, scatter_idx
+
+
+def init_compute_data(m, n, k, gindx, sindx, n_expts_tot, n_expts_act, act_dtype, weight_dtype, has_y_gammas, device="cuda"):
+    torch.manual_seed(0)
+    in_m = m * (n_expts_act if gindx is None else 1)
+    shape_x = (in_m, k)
+    x = alloc_rand(shape_x, device=device, dtype=act_dtype)
+    w = alloc_rand((n_expts_tot, k, n), device=device, dtype=weight_dtype)
+    bias = alloc_rand((n_expts_tot, n), device=device, dtype=torch.float32)
+    if has_y_gammas:
+        gamma = 2**torch.randint(-5, 0, (m * n_expts_act, ), device=device, dtype=torch.float32)
+    else:
+        gamma = None
+    return x, w, bias, gamma
+
+
+def dtype_str_to_torch(dtype_str: str) -> torch.dtype:
+    return torch.uint8 if dtype_str == "float4_e2m1" else getattr(torch, dtype_str)
+
+
+def assert_close(ref, tri, maxtol=None, rmstol=None, description="--", verbose=True):
+    if tri.dtype.itemsize == 1:
+        ref_as_type = ref.to(tri.dtype)
+        if ref.dtype == tri.dtype:
+            assert torch.all(ref_as_type == tri)
+            return
+        ref = ref_as_type
+
+    if ref.numel() == 0:
+        return
+
+    if maxtol is None:
+        maxtol = 2e-2
+    if rmstol is None:
+        rmstol = 4e-3
+    """
+    Compare reference values against obtained values.
+    """
+
+    # cast to float32:
+    ref = ref.to(torch.float32).detach()
+    tri = tri.to(torch.float32).detach()
+    assert ref.shape == tri.shape, f"Tensors must have same size {ref.shape=} {tri.shape=}"
+
+    # deal with infinite elements:
+    inf_mask_ref = torch.isinf(ref)
+    inf_mask_tri = torch.isinf(tri)
+    assert torch.equal(inf_mask_ref, inf_mask_tri), "Tensor must have same infinite elements"
+    refn = torch.where(inf_mask_ref, 0, ref)
+    trin = torch.where(inf_mask_tri, 0, tri)
+
+    # normalise so that RMS calculation doesn't overflow:
+    eps = 1.0e-30
+    multiplier = 1.0 / (torch.max(torch.abs(refn)) + eps)
+    refn *= multiplier
+    trin *= multiplier
+
+    ref_rms = torch.sqrt(torch.square(refn).mean()) + eps
+
+    rel_err = torch.abs(refn - trin) / torch.maximum(ref_rms, torch.abs(refn))
+    max_err = torch.max(rel_err).item()
+    rms_err = torch.sqrt(torch.square(rel_err).mean()).item()
+
+    if verbose:
+        print("%s maximum relative error = %s (threshold = %s)" % (description, max_err, maxtol))
+        print("%s RMS relative error = %s (threshold = %s)" % (description, rms_err, rmstol))
+
+    if max_err > maxtol:
+        bad_idxs = torch.nonzero(rel_err > maxtol)
+        num_nonzero = bad_idxs.size(0)
+        bad_idxs = bad_idxs[:1000]
+        print("%d / %d mismatched elements (shape = %s) at coords %s" %
+              (num_nonzero, rel_err.numel(), tuple(rel_err.shape), bad_idxs.tolist()))
+
+        bad_idxs = bad_idxs.unbind(-1)
+        print("ref values: ", ref[tuple(bad_idxs)].cpu())
+        print("tri values: ", tri[tuple(bad_idxs)].cpu())
+
+    assert max_err <= maxtol
+    assert rms_err <= rmstol
+
+
+# ---------------
+# unit tests
+# ---------------
+
+
+@dataclass
+class Case:
+    m: int
+    n: int
+    k: int
+    act_dtype_str: str
+    weight_dtype_str: str
+    n_expts_tot: int = 1
+    n_expts_act: int = 1
+    hbm_swizzling: bool = False
+
+
+@pytest.mark.parametrize(
+    ", ".join(f.name for f in fields(Case)),
+    [
+        tuple(getattr(case, f.name) for f in fields(Case)) for case in [
+            Case(32, 3072, 6144, "float8_e4m3fn", "mxfloat8_e4m3fn", 128, 4, hbm_swizzling=True),
+            Case(4096, 3072, 3072, "float8_e4m3fn", "mxfloat8_e4m3fn", 128, 4),
+            Case(16, 1024, 1024, "mxfloat8_e4m3fn", "mxfloat8_e4m3fn", 128, 4, hbm_swizzling=True),
+            Case(4096, 1024, 1024, "mxfloat8_e4m3fn", "mxfloat8_e4m3fn", 128, 4),
+            Case(16, 256, 256, "mxfloat8_e4m3fn", "mxfloat8_e4m3fn", 128, 4, hbm_swizzling=True),
+            Case(4096, 256, 256, "mxfloat8_e4m3fn", "mxfloat8_e4m3fn", 128, 4),
+            Case(1000, 704, 800, "mxfloat8_e4m3fn", "mxfloat8_e4m3fn", 8, 2),
+            Case(300, 400, 800, "mxfloat8_e4m3fn", "mxfloat8_e4m3fn", 8, 4),
+        ]
+    ],
+)
+@pytest.mark.parametrize("do_gather, do_scatter", [
+    (False, False),
+    (True, False),
+    (False, True),
+    (True, True),
+])
+@pytest.mark.parametrize("has_y_gammas", [False, True])
+@pytest.mark.parametrize("apply_swiglu", [False, True])
+@pytest.mark.parametrize("fused_quant", [False, True])
+def test_op(m, n, k, do_gather, do_scatter, has_y_gammas, apply_swiglu, fused_quant, 
+            n_expts_tot, n_expts_act, act_dtype_str, weight_dtype_str, hbm_swizzling, device="cuda"):
+
+    if "float8" in act_dtype_str and "mx" in weight_dtype_str and get_arch() != "gfx950":
+        pytest.skip("float8 x mx only supported on CDNA4")
+
+    if "float8_e4m3fnuz" in (weight_dtype_str, act_dtype_str) and get_arch() != "gfx942":
+        pytest.skip("float8_e4m3fnuz only tested on AMD CDNA3 Platform")
+
+    if hbm_swizzling:
+        if get_arch() != "gfx950":
+            pytest.skip("Scale preshuffling on AMD GPU has not been emulated on non-CDNA4 arch yet.")
+        if "mx" not in weight_dtype_str:
+            pytest.skip("Non-scale swizzling not supported on CDNA4 yet")
+        if n % 32 != 0 or k % (32 * 8) != 0:
+            pytest.skip(f"Shape {m}x{n}x{k} is not supported for scale swizzling on AMD GPU")
+
+    torch.manual_seed(0)
+
+    weight_mxfp = weight_dtype_str.startswith("mx")
+    if weight_mxfp:
+        weight_dtype_str = weight_dtype_str[2:]
+    act_mxfp8 = act_dtype_str.startswith("mx")
+    if act_mxfp8:
+        act_dtype_str = act_dtype_str[2:]
+
+    weight_dtype = dtype_str_to_torch(weight_dtype_str)
+    act_dtype = dtype_str_to_torch(act_dtype_str)
+    m, rdata, gindx, sindx = init_routing_data(m, n_expts_tot, n_expts_act, do_gather, do_scatter, device=device)
+    x_tri, w_tri, bias_tri, gammas = init_compute_data(m, n, k, gindx, sindx, n_expts_tot, n_expts_act,
+                                                                 torch.bfloat16 if act_mxfp8 else act_dtype,
+                                                                 torch.bfloat16,
+                                                                 has_y_gammas, device=device)
+    x_ref, w_ref, bias_ref = x_tri.clone(), w_tri.clone(), bias_tri.clone() 
+
+    # downcast to mxfp
+    w_tri, w_scale_tri = downcast_to_mxfp(w_tri, weight_dtype, axis=1)
+    w_ref = upcast_from_mxfp(w_tri, w_scale_tri, torch.bfloat16, axis=1)
+    if hbm_swizzling:
+        swizzle_mx_scale = "CDNA4_SCALE"
+        w_scale_tri = swizzle_scales(w_scale_tri)
+    else:
+        swizzle_mx_scale = None
+    
+    if act_mxfp8:
+        x_tri, x_mx_scales_tri = downcast_to_mxfp(x_tri, act_dtype, axis=-1)
+        x_ref = upcast_from_mxfp(x_tri, x_mx_scales_tri, torch.bfloat16, axis=-1)
+        x_static_scale = None
+        out_dtype = torch.bfloat16
+        maxtol = None
+        rmstol = None
+    else:
+        x_mx_scales_tri = None
+        x_static_scale = x_tri.abs().max().float() / 448.0 
+        x_tri = downcast_to_static_fp8(x_tri, x_static_scale)
+        out_dtype = torch.float8_e4m3fn
+        maxtol = 4e-1
+        rmstol = 4e-2
+
+
+    ref_y = moe_gemm_torch(x_ref, w_ref, bias_ref, rdata, gindx, sindx, gammas, apply_swiglu)
+    if not act_mxfp8 and fused_quant:
+        quant_static_scale = ref_y.abs().max().float() / 448.0 
+    else:
+        quant_static_scale = None
+    tri_y = moe_gemm_a8w8(x_tri, w_tri, x_mx_scales_tri, w_scale_tri, x_static_scale, quant_static_scale, bias_tri, rdata, gindx, sindx, gammas, swizzle_mx_scale, out_dtype, apply_swiglu)
+    if not act_mxfp8 and fused_quant:
+        tri_y = (tri_y.float() * quant_static_scale).to(ref_y.dtype)
+    assert_close(ref_y, tri_y, maxtol=maxtol, rmstol=rmstol)
